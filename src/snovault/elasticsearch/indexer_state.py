ef318aea41b5171e447f15a413daf62240f2b6da
caseylitton <caseylitton@gmail.com>
2018-09-05 10:46:08 -0700

    testing redinex all fix 2

--- src/snovault/elasticsearch/indexer_state.py
+++ src/snovault/elasticsearch/indexer_state.py
@@ -156,6 +156,7 @@ class IndexerState(object):
                 print('delete_objs', ["indexing"])
                 self.delete_objs(["indexing"])  # http://localhost:9201/snovault/meta/indexing
             self.put_obj(self.override, {self.title : 'reindex', 'all_uuids': True})
+            print('request_reindex all', self.get_obj(self.override))
 
         else:
             uuid_list = requested.split(',')
